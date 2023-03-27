19f4a72589af9c7f9ce83cf947a2b0c0ea1bac81
Anthony <aquinoanthony851@gmail.com>
2023-03-26 21:33:49 -0600

    DOM Elements

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+document.body.append(newHeader);
+
+newHeader.setAttribute("id", 'victory');
+
+newHeader.textContent = "Anthony is the champion";
